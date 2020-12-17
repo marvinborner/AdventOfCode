2a774b682f467ead8e8b2afc22274da1c5be0407
Marvin Borner <develop@marvinborner.de>
2020-12-17 11:49:26 +0100

    Better indenting I suppose

--- 2020/17/solve.c
+++ 2020/17/solve.c
@@ -36,30 +36,28 @@ int part_one(FILE *fp)
 		memset(new, 0, X_SIZE * Y_SIZE * Z_SIZE);
 		// LOL
 		for (int z = 1; z < Z_SIZE - 1; z++)
-			for (int y = 1; y < Y_SIZE - 1; y++)
-				for (int x = 1; x < X_SIZE - 1; x++)
-					for (int z2 = z - 1; z2 < z + 2; z2++)
-						for (int y2 = y - 1; y2 < y + 2; y2++)
-							for (int x2 = x - 1; x2 < x + 2; x2++)
-								if ((z != z2 || y != y2 ||
-								     x != x2) &&
-								    dat[z2][y2][x2])
-									new[z][y][x]++;
+		for (int y = 1; y < Y_SIZE - 1; y++)
+		for (int x = 1; x < X_SIZE - 1; x++)
+		for (int z2 = z - 1; z2 < z + 2; z2++)
+		for (int y2 = y - 1; y2 < y + 2; y2++)
+		for (int x2 = x - 1; x2 < x + 2; x2++)
+			if ((z != z2 || y != y2 || x != x2) && dat[z2][y2][x2])
+				new[z][y][x]++;
+
 		for (int z = 1; z < Z_SIZE - 1; z++)
-			for (int y = 1; y < Y_SIZE - 1; y++)
-				for (int x = 1; x < X_SIZE - 1; x++)
-					if ((new[z][y][x] == 2 && dat[z][y][x]) ||
-					    new[z][y][x] == 3) {
-						if (dat[z][y][x])
-							continue;
-						dat[z][y][x] = 1;
-						res++;
-					} else {
-						if (!dat[z][y][x])
-							continue;
-						dat[z][y][x] = 0;
-						res--;
-					}
+		for (int y = 1; y < Y_SIZE - 1; y++)
+		for (int x = 1; x < X_SIZE - 1; x++)
+			if ((new[z][y][x] == 2 && dat[z][y][x]) || new[z][y][x] == 3) {
+				if (dat[z][y][x])
+					continue;
+				dat[z][y][x] = 1;
+				res++;
+			} else {
+				if (!dat[z][y][x])
+					continue;
+				dat[z][y][x] = 0;
+				res--;
+			}
 	}
 
 	return res;
@@ -88,39 +86,31 @@ int part_two(FILE *fp)
 		memset(new, 0, X_SIZE * Y_SIZE * Z_SIZE);
 		// LOL LOL
 		for (int w = 1; w < W_SIZE - 1; w++)
-			for (int z = 1; z < Z_SIZE - 1; z++)
-				for (int y = 1; y < Y_SIZE - 1; y++)
-					for (int x = 1; x < X_SIZE - 1; x++)
-						for (int w2 = w - 1; w2 < w + 2; w2++)
-							for (int z2 = z - 1; z2 < z + 2; z2++)
-								for (int y2 = y - 1; y2 < y + 2;
-								     y2++)
-									for (int x2 = x - 1;
-									     x2 < x + 2; x2++)
-										if ((w != w2 ||
-										     z != z2 ||
-										     y != y2 ||
-										     x != x2) &&
-										    dat[w2][z2][y2]
-										       [x2])
-											new[w][z][y]
-											   [x]++;
+		for (int z = 1; z < Z_SIZE - 1; z++)
+		for (int y = 1; y < Y_SIZE - 1; y++)
+		for (int x = 1; x < X_SIZE - 1; x++)
+		for (int w2 = w - 1; w2 < w + 2; w2++)
+		for (int z2 = z - 1; z2 < z + 2; z2++)
+		for (int y2 = y - 1; y2 < y + 2; y2++)
+		for (int x2 = x - 1; x2 < x + 2; x2++)
+			if ((w != w2 || z != z2 || y != y2 || x != x2) && dat[w2][z2][y2][x2])
+				new[w][z][y][x]++;
+
 		for (int w = 1; w < W_SIZE - 1; w++)
-			for (int z = 1; z < Z_SIZE - 1; z++)
-				for (int y = 1; y < Y_SIZE - 1; y++)
-					for (int x = 1; x < X_SIZE - 1; x++)
-						if ((new[w][z][y][x] == 2 && dat[w][z][y][x]) ||
-						    new[w][z][y][x] == 3) {
-							if (dat[w][z][y][x])
-								continue;
-							dat[w][z][y][x] = 1;
-							res++;
-						} else {
-							if (!dat[w][z][y][x])
-								continue;
-							dat[w][z][y][x] = 0;
-							res--;
-						}
+		for (int z = 1; z < Z_SIZE - 1; z++)
+		for (int y = 1; y < Y_SIZE - 1; y++)
+		for (int x = 1; x < X_SIZE - 1; x++)
+			if ((new[w][z][y][x] == 2 && dat[w][z][y][x]) || new[w][z][y][x] == 3) {
+				if (dat[w][z][y][x])
+					continue;
+				dat[w][z][y][x] = 1;
+				res++;
+			} else {
+				if (!dat[w][z][y][x])
+					continue;
+				dat[w][z][y][x] = 0;
+				res--;
+			}
 	}
 
 	return res;
