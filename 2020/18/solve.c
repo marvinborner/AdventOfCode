214a554dcf606e1918e4c0cca22fa31d993c089c
Marvin Borner <develop@marvinborner.de>
2020-12-18 13:17:29 +0100

    Argh all test cases pass :O

---
+++ 2020/18/solve.c
@@ -0,0 +1,86 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+long add_or_mul(long *res, int num, int mul)
+{
+	if (mul == 0)
+		*res += num;
+	else
+		*res *= num;
+	return *res;
+}
+
+long evil(char *str)
+{
+	long res = 0;
+	int mul = 0, skip = 0, brace_cnt = 0;
+	for (char *p = str; *p; p++) {
+		if (skip) {
+			if (*p == '(') {
+				brace_cnt++;
+			} else if (*p == ')' && !(brace_cnt--)) {
+				brace_cnt = 0;
+				skip = 0;
+			}
+			continue;
+		}
+
+		if (*p == ' ') {
+			continue;
+		} else if (*p == '(') {
+			add_or_mul(&res, evil(p + 1), mul);
+			skip = 1;
+		} else if (*p == ')') {
+			break; // That's why I don't use switch..case
+		} else if (*p == '*') {
+			mul = 1;
+		} else if (*p == '+') {
+			mul = 0;
+		} else if (isdigit(*p)) {
+			add_or_mul(&res, *p - '0', mul);
+		}
+	}
+	return res;
+}
+
+int part_one(FILE *fp)
+{
+	int res = 0;
+
+	char *line = NULL;
+	size_t len;
+	while (getline(&line, &len, fp) != -1) {
+		int a = evil(line);
+		printf("%d\n", a);
+		res += a;
+	}
+
+	return res;
+}
+
+int part_two(FILE *fp)
+{
+	int res = 0;
+
+	return res;
+}
+
+int main(int argc, char *argv[])
+{
+	FILE *fp = fopen("input", "r");
+	if (!fp)
+		exit(EXIT_FAILURE);
+
+	clock_t tic = clock();
+	printf("%lu\n", part_one(fp));
+	rewind(fp);
+	printf("%lu\n", part_two(fp));
+	clock_t toc = clock();
+	printf("TIME: %f seconds\n", (double)(toc - tic) / CLOCKS_PER_SEC);
+
+	fclose(fp);
+	return 0;
+}
