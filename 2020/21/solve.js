f4adb9df46a64f274afd9decec465e2d89a5e516
Marvin Borner <develop@marvinborner.de>
2020-12-21 16:42:59 +0100

    Added part 2 (golfed it a bit)

--- 2020/21/solve.js
+++ 2020/21/solve.js
@@ -2,12 +2,10 @@ const { _, performance } = require("perf_hooks");
 const fs = require("fs");
 const d = fs.readFileSync("input", "utf8").split("\n");
 
-function partOne() {
-c=" (contains ";f=d.map(x=>({i:x.split(c)[0].split(" "),a:x.split(c)[1].split(")")[0].split(", ")}));a=new Map();for(g of f)for(b of g.a)a.set(b,new Set(a.get(b)?[...g.i].filter(x=>a.get(b).has(x)):g.i));return f.map(x=>x.i).flat().filter(x=>!new Set([...a.values()].reduce((a,b)=>[...a,...b],[])).has(x)).length;
-}
-
 const tic = performance.now();
-console.log(partOne());
-//console.log(partTwo());
+
+// YAYY
+(()=>{c=" (contains ";f=d.map(x=>({i:x.split(c)[0].split(" "),a:x.split(c)[1].split(")")[0].split(", ")}));a=new Map();for(g of f)for(b of g.a)a.set(b,new Set(a.get(b)?[...g.i].filter(x=>a.get(b).has(x)):g.i));console.log(f.map(x=>x.i).flat().filter(x=>!new Set([...a.values()].reduce((a,b)=>[...a,...b],[])).has(x)).length);o=Array.from(a,([b,i])=>({b,i}));let n;do{n=!1;for(b of o)if(b.i.size>1){b.i=new Set([...b.i].filter(x=>!o.filter(x=>x.i.size==1).map(x=>[...x.i][0]).includes(x)));n=!0}}while(n)console.log(o.sort((a,b)=>a.b.localeCompare(b.b)).map(x=>[...x.i][0]).join(','))})()
+
 const toc = performance.now();
 console.log("TIME: " + ((toc - tic) / 1000).toFixed(6) + " seconds");
