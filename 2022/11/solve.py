4af6738bb5665a299d590d6f04d9bf61944b9a28
Marvin Borner <develop@marvinborner.de>
2022-12-11 14:49:28 +0100

    Installed a formatter hmm

--- 2022/11/solve.py
+++ 2022/11/solve.py
@@ -1,13 +1,28 @@
-data = [[line.strip() for line in dat.split("\n")] for dat in open("input").read().split("\n\n") if dat != '']
+data = [
+    [line.strip() for line in dat.split("\n")]
+    for dat in open("input").read().split("\n\n")
+    if dat != ""
+]
+
 
 def solve(part):
-    res = 0
-
-    monkeys = [{"items": [], "op": None, "divisibility": 1, "true": 0, "false": 0, "inspected": 0} for i in range(len(data))]
+    monkeys = [
+        {
+            "items": [],
+            "op": None,
+            "divisibility": 1,
+            "true": 0,
+            "false": 0,
+            "inspected": 0,
+        }
+        for i in range(len(data))
+    ]
 
     # setup monkeys
-    for i,block in enumerate(data):
-        monkeys[i]["items"] = [int(item) for item in block[1].split(": ")[1].split(", ")]
+    for i, block in enumerate(data):
+        monkeys[i]["items"] = [
+            int(item) for item in block[1].split(": ")[1].split(", ")
+        ]
         monkeys[i]["op"] = block[2].split(": ")[1].replace("new = ", "")
         monkeys[i]["divisibility"] = int(block[3].split(" ")[-1])
         monkeys[i]["true"] = int(block[4].split(" ")[-1])
@@ -15,11 +30,13 @@ def solve(part):
 
     if part == 2:
         # evil muhahahaa
-        common = eval("*".join(str(monkey["divisibility"]) for monkey in monkeys))
+        common = eval(
+            "*".join(str(monkey["divisibility"]) for monkey in monkeys)
+        )
 
     for r in range(20 if part == 1 else 10000):
         current = 0
-        while True: # new round
+        while True:  # new round
             if current >= len(monkeys):
                 break
 
@@ -38,6 +55,7 @@ def solve(part):
 
     bizz = sorted([monkey["inspected"] for monkey in monkeys])
     return bizz[-1] * bizz[-2]
+
 
 print(f"Part 1: {solve(1)}")
 print(f"Part 2: {solve(2)}")
