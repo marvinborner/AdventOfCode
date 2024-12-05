6e53c274e8106f3c88ab9ac1cad40cfc2e9e28cb
Marvin Borner <git@marvinborner.de>
2024-12-05 06:34:04 +0100

    Yet, minor cleanup

--- 2024/05/solve.py
+++ 2024/05/solve.py
@@ -7,19 +7,14 @@ numss = [[int(c) for c in l.split(",")] for l in L[1].strip().split("\n")]
 
 
 def check(nums):
-    pos = (-1, -1)
-    ordered = True
     for i, a in enumerate(nums):
         for j, b in enumerate(nums[i:]):
             if [b, a] in ordering:
-                ordered = False
-                pos = (i, i + j)
-                break
-        if not ordered:
-            break
-    return (pos, ordered)
+                return ((i, i + j), False)
+    return (None, True)
 
 
+# "bubble sort"
 def fix(nums):
     while True:
         pos, ordered = check(nums)
@@ -32,12 +27,12 @@ def solve():
     res1 = 0
     res2 = 0
 
-    for n, nums in enumerate(numss):
+    for nums in numss:
         _, ordered = check(nums)
         if ordered:
             res1 += nums[len(nums) // 2]
         else:
-            fixed = fix(nums.copy())
+            fixed = fix(nums)
             res2 += fixed[len(fixed) // 2]
 
     print(res1)
